9e9e0e76e879e5b66f987bde23a80fc8bebfcb76
Daniel <dyankiver@gmail.com>
2020-12-10 10:50:27 -0500

    final

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,6 +3,8 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
+      #create a new migration to the table active that is a bolean 
+      # create 002 add active to syntax 
 
       t.timestamps null: false
     end
